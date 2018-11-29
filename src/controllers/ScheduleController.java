6081a836b2cee67f7995c0616a455c06f0830f98
mockl <mockl@66.71.17.14>
2018-11-29 16:09:39 -0500

    changed schedule appointment UI

--- src/controllers/ScheduleController.java
+++ src/controllers/ScheduleController.java
@@ -34,6 +34,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import models.AppointmentStore;
 
 /**
@@ -58,6 +59,8 @@ public class ScheduleController implements Initializable {
     private TableView<ScheduleTableEntry> scheduledTable;
     @FXML
     private ChoiceBox timeSelect;
+    @FXML
+    private Button homeButton;
 
     @FXML
     private void onPhysicianSelect(ActionEvent event) {
@@ -133,6 +136,7 @@ public class ScheduleController implements Initializable {
             Parent root1 = (Parent) fxmlLoader.load();
             Stage stage = new Stage();
             stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initStyle(StageStyle.UNDECORATED);
             stage.setTitle("AlphaCare");
             stage.setScene(new Scene(root1));
             stage.show();
@@ -147,5 +151,6 @@ public class ScheduleController implements Initializable {
         HomeController homeController = new HomeController();
         homeController.startHomeUI();
     }
+
 
 }
